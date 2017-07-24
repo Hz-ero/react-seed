ba1005b3f89bafc05fc9b8ef4e3a0bff662e7918
lijiaqiang <lijiaqiang@outlook.com>
2017-07-24 21:36:43 +0800

    fix

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,7 +4,7 @@ import BTCchartContainer from './BTCchartContainer'
 
 const App = () => (
     <div>
-        <Link to="/about">About</Link>
+        { /*<Link to="/about">About</Link>*/ }
         <BTCchartContainer />
     </div>
 )
